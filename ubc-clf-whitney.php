ad117ae89c3300abef1a201a5b1fd83a79223ee3
Flynn <fsquaredesign@gmail.com>
2026-06-12 09:37:50 -0700

    Updated version number

--- ubc-clf-whitney.php
+++ ubc-clf-whitney.php
@@ -6,7 +6,7 @@
  * Plugin Name:       UBC CLF Whitney webfont
  * Plugin URI:        http://clf.ubc.ca
  * Description:       Add CLF Whitney webfont CSS request. <strong>Note: Required registration</strong>. Please sign up on <a href="http://brand.ubc.ca/font-request-form/" target="_blank">UBC Brand</a> website.
- * Version:           1.1
+ * Version:           1.2
  * Author:            Michael Kam & Flynn O'Connor
  * Author URI:
  * License:           GPL-2.0+
